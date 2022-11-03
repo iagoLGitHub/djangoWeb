205733d9a0f30ad6a8cc4c2490efd8a82c80f7b4
iagoLGitHub <arnajenkins86@gmail.com>
2022-11-03 18:25:56 +0100

    django inicial preparado

--- djangoWeb/misitio/models.py
+++ djangoWeb/misitio/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Cliente(models.Model):
+    dni = models.CharField(max_length=8)
+    nombre = models.CharField(max_length=150, blank=False, null=False)
+    fecha_alta = models.DateTimeField("Fecha de alta", blank=False, null=False)
+    direccion = models.CharField(max_length=150, blank= False, null=True)
+    mobile = models.CharField(max_length=150, blank=False, null=True)
+
+
--- djangoWeb/misitio/views.py
+++ djangoWeb/misitio/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from .models import Cliente
+from django.utils import timezone
+# Create your views here.
+
+
+def clientes_list(request):
+    clientes = Cliente.objects.filter(fecha_alta__lte=timezone.now()).order_by('fecha_alta')
+    return render(request, 'misitio/clientes_list.html', {'clientes': clientes})
